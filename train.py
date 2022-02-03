93fe56f56c1added4aa2a24b6007200bceeda169
FrischJulien <jfrisch59@gmail.com>
2022-02-03 13:24:18 +0100

    option to let "no" for restore_path

--- train.py
+++ train.py
@@ -41,11 +41,15 @@ def train(script_name,restore_path):
         print("1 minute wait ended")
         shutil.copyfile(os.path.join(scripts_path,script_name), "/workspace/TTS/train_script.py")
         print("{} copied".format(script_name))
-        shutil.copyfile(restore_path, "/workspace/TTS/copied_checkpoint.pth.tar")
-        print("{} copied".format(restore_path))
+        if restore_path!="no":
+            shutil.copyfile(restore_path, "/workspace/TTS/copied_checkpoint.pth.tar")
+            print("{} copied".format(restore_path))
         os.environ["CUDA_VISIBLE_DEVICES"]="0, 1 , 2, 3"
         print("Cuda visible devices put to 4")
-        os.system("python3 distribute-Copy.py --script train_script.py --restore_path copied_checkpoint.pth.tar")
+        if restore_path!="no":
+            os.system("python3 distribute-Copy.py --script train_script.py --restore_path copied_checkpoint.pth.tar")
+        else:
+            os.system("python3 distribute-Copy.py --script train_script.py")
         print('Training complete.')
     except Exception as e:
         # Write out an error file. This will be returned as the failureReason in the
